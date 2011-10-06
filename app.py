2b328bfffbbf402691a27fe9dfec7b7a0179b967
shinvee <shinvee@gmail.com>
2011-10-06 16:18:40 +0900

    Modified for heroku.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def js():
     return redirect(url_for('static', filename='steve.js'))
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run()
